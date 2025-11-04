983863d9d28d67de0b3f76f6c1e52e0170c83d19
Justino SOares <justinocsoares123@gmail.com>
2025-11-04 15:58:32 +0100

    bases instalations

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+app.use(express_1.default.json());
+// Middleware
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)());
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Sample route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
