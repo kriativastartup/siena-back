1998a55b55e15b1c81f3b1424390aa5fc2ff59e6
jsoares <jsoares@bulir.com>
2026-02-22 20:19:53 +0100

    feat: list school

--- src/routers/escola.ts
+++ src/routers/escola.ts
@@ -9,8 +9,8 @@ import * as dto from "../services/escolas/dto/escola.dto";
 
 router.post('/create', autho.verifyAuthenticationSuperAdmin, validate(dto.CreateEscolaSchema), controller.createEscola);
 router.get('/all', autho.verifyAuthenticationSuperAdmin, controller.getEscolas);
-router.get('/each/:schoolId', autho.verifyAuthenticationAdmin, controller.getEscolaById);
-router.put('/update/:schoolId', autho.verifyAuthenticationAdmin, controller.updateEscola);
-router.delete('/delete/:schoolId', autho.verifyAuthenticationSuperAdmin, controller.deleteEscola);
+router.get('/each/:escola_id', autho.verifyAuthenticationAdmin, controller.getEscolaById);
+router.put('/update/:escola_id', autho.verifyAuthenticationAdmin, controller.updateEscola);
+router.delete('/delete/:escola_id', autho.verifyAuthenticationSuperAdmin, controller.deleteEscola);
 
 export default router;
